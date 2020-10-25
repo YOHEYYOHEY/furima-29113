8cb17fa8a7cddc4706a5c97bd1bb264ea8d69626
YOHEYYOHEY <yohey33d0v0v@gmail.com>
2020-10-25 20:42:23 +0900

    Basic認証の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   private
 
   def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
+    authenticate_or_request_with_http_basic_ do |username, password|
       username == 'yohey' && password == '2600'
     end
   end  
