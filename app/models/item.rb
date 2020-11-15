030e6a3cf7d39a996a5722d145db21d7ce584a51
YOHEYYOHEY <yohey33d0v0v@gmail.com>
2020-11-15 21:10:01 +0900

    商品詳細・配送関係エラーメッセージ実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
   validates :delivery_day_id,presence: true
   validates :price,presence:true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
-  # validates :image, presence: true
+  validates :image, presence: true
 
   belongs_to :user
   has_one :purchase
