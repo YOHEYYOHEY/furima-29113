32a07e9286d06902f30e6b5f82cc1ce779886873
YOHEYYOHEY <yohey33d0v0v@gmail.com>
2020-11-15 19:14:24 +0900

    エラーメッセージ実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,14 +9,19 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
-  end
+    @item = Item.new(item_params)
+    if @item.save
 
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :image)
+    params.require(:item).permit(:name, :introduction, :category_id, :status_id, :delivery_charge_id, :delivery_area_id, :delivery_day_id, :price)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,14 @@ class Item < ApplicationRecord
 
   validates :name,presence: true, length: { maximum: 40 }
   validates :introduction,presence: true, length: { maximum: 1000 }
-  validates :category,presence: true
+  validates :category_id,presence: true
   validates :status_id,presence: true
   validates :delivery_charge_id,presence: true
   validates :delivery_area_id,presence: true
   validates :delivery_day_id,presence: true
   validates :price,presence:true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
-  validates :image, presence: true
+  # validates :image, presence: true
 
   belongs_to :user
   has_one :purchase
