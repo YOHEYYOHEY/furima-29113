aa4d79b0a252f7c868ef9de6e249de16a3771c55
YOHEYYOHEY <yohey33d0v0v@gmail.com>
2020-12-04 06:33:00 +0900

    商品情報編集機能修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -20,16 +21,23 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
+    @item.update(item_params)
+    if @item.save
+
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 
